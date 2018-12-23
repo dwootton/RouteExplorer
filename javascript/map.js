02b3cec2abb6e050752cb7615c3b03d59503923f
Dylan Wootton <wootton.dylan@gmail.com>
2018-12-22 17:06:32 -0700

    added drawing functionality and properly alert points on click

--- javascript/map.js
+++ javascript/map.js
@@ -71,24 +71,10 @@ class AQMap {
 		        .call(colorLegend);
 
 		        console.log(testLegend); 
+		    this.shapeDrawer = new shapeDrawer(this.myMap);
+			
 
-		let drawingManager = new google.maps.drawing.DrawingManager({
-          drawingMode: google.maps.drawing.OverlayType.MARKER,
-          drawingControl: true,
-          drawingControlOptions: {
-            position: google.maps.ControlPosition.TOP_CENTER,
-            drawingModes: ['marker', 'circle', 'polygon', 'polyline', 'rectangle']
-          },
-          circleOptions: {
-            fillColor: '#ffff00',
-            fillOpacity: 1,
-            strokeWeight: 5,
-            clickable: false,
-            editable: true,
-            zIndex: 1
-          }
-        });
-        drawingManager.setMap(this.myMap);
+
  
 		
 
